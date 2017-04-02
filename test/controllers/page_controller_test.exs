6ac96c164cbe274bbd23259eec6cfdb5b097ea14
Joe Peck <joe.peck@scimedsolutions.com>
2017-04-02 12:14:07 -0400

    Update Page test

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule MegaMerchant.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to MegaMerchant!"
   end
 end
